9176918a2df1c199e92346df8724ac290fc2e7b5
Naoki Inomata <soramoyou04@live.jp>
2023-04-05 22:26:43 +0900

    プロジェクト ファイルを追加します。

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TeamCalender;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
